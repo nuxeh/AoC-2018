e0db89a0756016ad51359cb90abf379707d59afa
Edward Cragg <drq.11235@gmail.com>
2018-12-01 16:00:38 +0000

    Get solutions with nim

--- 1/do.nim
+++ 1/do.nim
@@ -1,10 +1,31 @@
 import streams
+import strutils
+import tables
+import terminal
 
 var
-  fs = newFileStream("input.txt", fmRead)
+  file = newFileStream("input.txt", fmRead)
   line = ""
+  sum = 0
+  calibrations = newSeq[int]()
+  sum2 = 0
+  freq = initTable[int, int]()
 
-if not isNil(fs):
-  while fs.readLine(line):
-    echo line
-  fs.close()
+if not isNil(file):
+  while file.readLine(line):
+    var i = parseInt(line)
+    sum += i
+    calibrations.add(i)
+  file.close()
+  styledEcho fgGreen, $sum
+
+while true:
+  for j in calibrations:
+    sum2 += j
+    if freq.hasKey(sum2):
+      freq[sum2] = freq[sum2] + 1
+    else:
+      freq[sum2] = 1
+    if freq[sum2] == 2:
+      echo sum2
+      quit()
--- 1/do.sh
+++ 1/do.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 awk -f do.awk input.txt
+nim compile -d:release --run do.nim
