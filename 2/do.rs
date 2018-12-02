9fb740d326d2761fa7f2728e289e8f869269d700
Edward Cragg <drq.11235@gmail.com>
2018-12-02 11:42:06 +0000

    Get histogram of letters
    {'x': 2, 'd': 0, 'y': 0, 'm': 0, 'o': 0, 'l': 0, 'e': 0, 't': 0, 'c': 0, 'j': 0, 'a': 0, 'i': 0, 'w': 0, 'h': 1, 'q': 0, 'g': 0, 'u': 0, 'r': 0, 'b': 0, 'v': 0, 's': 0, 'z': 0, 'p': 0}
    {'k': 0, 'w': 0, 's': 0, 'i': 0, 'j': 1, 'q': 0, 'y': 0, 'x': 0, 'h': 0, 'c': 0, 'm': 0, 'v': 1, 'n': 0, 'g': 1, 'f': 0, 'e': 0, 'a': 0, 'b': 0, 'o': 0, 't': 0, 'd': 0, 'l': 0, 'p': 0}
    {'f': 0, 't': 1, 'd': 0, 'i': 0, 'p': 1, 'h': 0, 'j': 0, 'e': 0, 'n': 0, 'x': 0, 'w': 1, 'o': 0, 'g': 0, 's': 0, 'y': 0, 'r': 0, 'm': 0, 'c': 0, 'u': 0, 'l': 0, 'b': 0, 'q': 0, 'a': 0}
    {'o': 0, 'a': 0, 'm': 1, 'f': 0, 'z': 0, 'i': 0, 'r': 0, 'q': 1, 'b': 0, 'g': 0, 't': 0, 'd': 0, 's': 1, 'u': 0, 'c': 0, 'y': 0, 'p': 0, 'e': 0, 'x': 0, 'j': 0, 'k': 0, 'n': 0, 'h': 0}
    {'x': 0, 'o': 1, 'y': 0, 'r': 0, 'l': 0, 't': 0, 'q': 0, 'u': 0, 'n': 0, 'm': 0, 'c': 0, 'g': 0, 'i': 0, 'd': 0, 'e': 0, 'k': 0, 'p': 0, 'f': 0, 'b': 1, 'h': 0, 'z': 0, 'a': 1, 'j': 0}
    {'n': 0, 'c': 1, 'm': 1, 'a': 0, 'b': 0, 'o': 0, 'k': 0, 'l': 0, 'e': 0, 's': 0, 'x': 0, 'v': 0, 'p': 1, 'u': 0, 'r': 0, 'i': 0, 'f': 0, 't': 0, 'y': 0, 'g': 0, 'w': 0, 'h': 0, 'z': 0}

--- 2/do.rs
+++ 2/do.rs
@@ -1,8 +1,20 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap_or(String::new());
     for line in input.lines() {
-        println!("{}", line);
+        let hist = line
+            .chars()
+            .fold(HashMap::new(), |mut hist: HashMap<char, u32>, a| {
+                if hist.contains_key(&a) {
+                    let count = hist.get_mut(&a).unwrap();
+                    *count += 1;
+                } else {
+                    hist.insert(a, 0);
+                }
+                hist
+            });
+        println!("{:?}", hist);
     }
 }
