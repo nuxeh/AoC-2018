23d1ff3b5cd825739c20cdb9c13860bc59dd00b4
Edward Cragg <drq.11235@gmail.com>
2018-12-03 12:02:45 +0000

    Replicate test result

--- 3/do.nim
+++ 3/do.nim
@@ -18,10 +18,12 @@ import re
 
 var
   filename = ""
+  dim = 2000
 
 let args = docopt(doc, version = "0.1.0")
 if args["--test"]:
   filename = "test.txt"
+  dim = 40
 else:
   filename = "input.txt"
 
@@ -35,20 +37,45 @@ if not isNil(file):
   while file.readLine(line):
     var matches: array[5, string]
     if match(line, re"#(\d*) @ (\d*),(\d*): (\d*)x(\d*)$", matches, 0):
-      for s in matches:
-        echo s
-    var
-      left = parseInt(matches[1]) + 1
-      top = parseInt(matches[2]) + 1
-      width = parseInt(matches[3])
-      height = parseInt(matches[4])
+      var
+        left = parseInt(matches[1])
+        top = parseInt(matches[2])
+        width = parseInt(matches[3])
+        height = parseInt(matches[4])
+        x = left
+        y = top
 
-    for x in left..(left+width):
-      for y in top..(top+height):
-        if fabric[x][y]:
-          overlap++
-        fabric[x][y] = true
+      while x < left + width:
+        y = top
+        while y < top + height:
+          if fabric[x][y]:
+            overlap += 1
+          fabric[x][y] = true
+          y += 1
+        x += 1
+
+#    for x in range[left..(left+width)]:
+#      for y in range[top..(top+height)]:
+#        if fabric[x][y]:
+#          overlap++
+#        fabric[x][y] = true
 
   file.close()
 
-  echo $overlap
+var
+  x = 0
+  y = 0
+
+if args["--test"]:
+  while y < dim:
+    x = 0
+    while x < dim:
+      var c = "."
+      if fabric[x][y]:
+        c = "#"
+      stdout.write c
+      x += 1
+    stdout.write "\n"
+    y += 1
+
+echo $overlap
