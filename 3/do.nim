24891485e583fcb5edd33c4bc8b1728f0b07bfbd
Edward Cragg <drq.11235@gmail.com>
2018-12-03 09:36:51 +0000

    Use regex

--- 3/do.nim
+++ 3/do.nim
@@ -14,6 +14,7 @@ import streams
 import strutils
 import tables
 import terminal
+import re
 
 var
   filename = ""
@@ -28,16 +29,26 @@ var
   file = newFileStream(filename, fmRead)
   line = ""
   fabric: array[2000, array[2000, bool]]
+  overlap = 0
 
 if not isNil(file):
   while file.readLine(line):
+    var matches: array[5, string]
+    if match(line, re"#(\d*) @ (\d*),(\d*): (\d*)x(\d*)$", matches, 0):
+      for s in matches:
+        echo s
     var
-      tokens = splitWhitespace(line)
-      top = tokens[2]
-      left = 0
-      width = 0
-      height = 0
-    for t in tokens:
-      echo $t
+      left = parseInt(matches[1]) + 1
+      top = parseInt(matches[2]) + 1
+      width = parseInt(matches[3])
+      height = parseInt(matches[4])
+
+    for x in left..(left+width):
+      for y in top..(top+height):
+        if fabric[x][y]:
+          overlap++
+        fabric[x][y] = true
+
   file.close()
 
+  echo $overlap
