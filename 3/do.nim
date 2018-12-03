6d96f0a650caf6ccf89712a0e63b24d7e2bccedc
Edward Cragg <drq.11235@gmail.com>
2018-12-03 12:38:55 +0000

    Read lines into a seq

--- 3/do.nim
+++ 3/do.nim
@@ -30,6 +30,7 @@ else:
 var
   file = newFileStream(filename, fmRead)
   line = ""
+  data = newSeq[string]()
   fabric: array[5000, array[5000, int]]
   overlap = 0
   clean_id = -1
@@ -37,31 +38,35 @@ var
 
 if not isNil(file):
   while file.readLine(line):
-    var matches: array[5, string]
-    if match(line, re"#(\d*) @ (\d*),(\d*): (\d*)x(\d*)$", matches, 0):
-      var
-        left = parseInt(matches[1])
-        top = parseInt(matches[2])
-        width = parseInt(matches[3])
-        height = parseInt(matches[4])
-        x = left
-        y = top
-        clean = true
+    data.add(line)
+  file.close()
 
-      while x < left + width:
-        y = top
-        while y < top + height:
-          if fabric[x][y] > 0:
-            clean = false
-          if fabric[x][y] == 1:
-            overlap += 1
-          fabric[x][y] += 1
-          y += 1
-        x += 1
+for line in data:
+  var matches: array[5, string]
+  if match(line, re"#(\d*) @ (\d*),(\d*): (\d*)x(\d*)$", matches, 0):
+    var
+      left = parseInt(matches[1])
+      top = parseInt(matches[2])
+      width = parseInt(matches[3])
+      height = parseInt(matches[4])
+      x = left
+      y = top
+      clean = true
 
-      if clean:
-         clean_id = parseInt(matches[0])
-         clean_area = width * height
+    while x < left + width:
+      y = top
+      while y < top + height:
+        if fabric[x][y] > 0:
+          clean = false
+        if fabric[x][y] == 1:
+          overlap += 1
+        fabric[x][y] += 1
+        y += 1
+      x += 1
+
+    if clean:
+       clean_id = parseInt(matches[0])
+       clean_area = width * height
 
 #    for x in range[left..(left+width)]:
 #      for y in range[top..(top+height)]:
@@ -70,8 +75,6 @@ if not isNil(file):
 #        fabric[x][y] = true
 
 # TODO: ranges/for loops?!
-
-  file.close()
 
 var
   x = 0
