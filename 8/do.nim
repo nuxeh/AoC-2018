e8631ffab1adaf67da8b6105738d52014e3a9a9b
Edward Cragg <drq.11235@gmail.com>
2018-12-08 10:37:37 +0000

    Parse ints into array

---
+++ 8/do.nim
@@ -0,0 +1,53 @@
+let doc = """
+Advent of code {}, day {}
+
+Usage:
+  day{} [options] [<input>]
+
+Options:
+  -h --help       Show this help message.
+  -v --verbose    Show extra information.
+  -t --test       Use test data
+"""
+import docopt
+import streams
+import strutils
+import tables
+import terminal
+import re
+import sequtils
+import sets
+
+var
+  filename = ""
+
+let args = docopt(doc, version = "0.1.0")
+if args["--test"]:
+  filename = "test.txt"
+else:
+  filename = "input.txt"
+
+type
+  Entry = tuple[
+    id: int,
+    left: int,
+    top: int,
+    width: int,
+    height: int,
+  ]
+
+var
+  file = newFileStream(filename, fmRead)
+  line = ""
+  data_s = newSeq[string]()
+  data = newSeq[int]()
+
+if not isNil(file):
+  while file.readLine(line):
+    data_s = splitWhitespace(line)
+  file.close()
+
+data = data_s.map(proc(s: string): int = parseInt(s))
+
+echo $data
+
