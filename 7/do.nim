ebe09aa6332a24b39b62e6d486b039917a47129d
Edward Cragg <drq.11235@gmail.com>
2018-12-08 10:27:56 +0000

    Get solution for part 1
    @[]
    @[]
    @['S']
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    @[]
    {}
    @['G', 'R', 'T', 'A', 'H', 'K', 'L', 'Q', 'V', 'Y', 'W', 'X', 'M', 'U', 'B', 'C', 'Z', 'P', 'I', 'J', 'F', 'E', 'D', 'N', 'S', 'O']
    GRTAHKLQVYWXMUBCZPIJFEDNSO

--- 7/do.nim
+++ 7/do.nim
@@ -64,6 +64,7 @@ proc find_root(): char =
       break
     stdout.write '\n'
 
+#[
 var
   root = find_root()
   stack = newSeq[char]()
@@ -90,3 +91,45 @@ pop_dependency(root)
 echo $stack
 
 echo $('C' > 'B')
+]#
+
+#[
+L ← Empty list that will contain the sorted elements
+S ← Set of all nodes with no incoming edge
+while S is non-empty do
+    remove a node n from S
+    add n to tail of L
+    for each node m with an edge e from n to m do
+        remove edge e from the graph
+        if m has no other incoming edges then
+            insert m into S
+if graph has edges then
+    return error   (graph has at least one cycle)
+else
+    return L   (a topologically sorted order)
+]#
+
+var
+  L = newSeq[char]()  # ← Empty list that will contain the sorted elements
+  S = initSet[char]() # ← Set of all nodes with no incoming edge
+
+for t in tasks:
+  if not deps.contains(t):
+    S.incl(t)
+
+echo "no incoming edges: " & $S
+
+while len(S) > 0:
+  let n = S.pop()
+  L.add(n)
+  for k, d in deps:
+    echo $d
+    if d.contains(n):
+      let i = d.find(n)
+      deps[k].delete(i)
+      if len(deps[k]) == 0:
+        S.incl(k)
+
+echo $S
+echo $L
+echo $L.join()
