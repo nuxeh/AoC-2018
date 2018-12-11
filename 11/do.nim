af8ca84e5bbe448a5648be128ef526da46ee5da7
Edward Cragg <drq.11235@gmail.com>
2018-12-11 07:39:18 +0000

    Create grid data

---
+++ 11/do.nim
@@ -0,0 +1,50 @@
+let doc = """
+Advent of code {}, day {}
+
+Usage:
+  day{} [options] [<input>]
+
+Options:
+  -h --help       Show this help message.
+  -v --verbose    Show extra information.
+  -t --test       Use test points
+"""
+import docopt
+import streams
+import strutils
+import tables
+import terminal
+import re
+import sequtils
+import sets
+
+var
+  filename = ""
+  input = 1309
+  grid: array[300, array[300, int]]
+
+let args = docopt(doc, version = "0.1.0")
+if args["--test"]:
+  input = 18
+
+proc calc(x, y: int): int =
+  var
+    rack_id = x + 10
+    power_level: int = rack_id * y
+
+  power_level += input
+  power_level *= rack_id
+  power_level = power_level div 100
+  power_level = power_level mod 10
+  power_level -= 5
+
+  result = power_level
+
+for y in low(grid)..high(grid):
+  for x in low(grid[y])..high(grid[y]):
+    grid[x][y] = calc(x, y)
+
+for y in low(grid)..high(grid):
+  for x in low(grid[y])..high(grid[y]):
+    stdout.write $grid[x][y] & " "
+  stdout.write '\n'
