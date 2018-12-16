249358ee96fd0f424317c72c5d2b51b04498a580
Edward Cragg <drq.11235@gmail.com>
2018-12-16 01:00:10 +0000

    Print digits
    998
    99
    9
    1
    0
    1
    2
    3
    1
    2
    3
    7
    9
    8
    1
    2
    3
    0
    9
    8
    1
    3
    2

--- 14/test.nim
+++ 14/test.nim
@@ -1,6 +1,18 @@
+import strutils
+
 var
   sum = 998
 
 while sum > 0:
   echo $sum
   sum = sum div 10
+
+proc printDigits(tot: int64) =
+  var
+    st = $tot
+
+  for c in st:
+    echo $c
+
+printDigits(10)
+printDigits(123123798123098132)
