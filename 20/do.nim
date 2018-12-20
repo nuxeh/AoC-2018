efc24ccc532f2fe6c1003f62afbe0928e46644db
Edward Cragg <drq.11235@gmail.com>
2018-12-20 12:45:01 +0000

    Parse inputs

---
+++ 20/do.nim
@@ -0,0 +1,58 @@
+let doc = """
+Advent of code 2018, day 20: A Regular Map
+
+Usage:
+  do [options] [<input>]
+
+Options:
+  -h --help       Show this help message.
+  -v --verbose    Show extra information.
+  -t --test       Use test points
+  --part2         Process for part 2
+"""
+import re
+import docopt
+import terminal
+import streams
+import strutils
+import sequtils
+import algorithm
+import tables
+import sets
+import lists
+import typetraits
+import strformat
+
+var
+  filename = ""
+
+let args = docopt(doc, version = "0.1.0")
+if args["--test"]:
+  filename = "test.txt"
+else:
+  filename = "input.txt"
+
+if args["<input>"]:
+  filename = $args["<input>"]
+
+type
+  ClayType = enum
+    Vertical,
+    Horizontal
+
+  Clay = object
+    kind: ClayType
+    x, x2, y, y2: int
+
+var
+  file = newFileStream(filename, fmRead)
+  line = ""
+  inputData: string
+
+if not isNil(file):
+  while file.readLine(line):
+    inputData = line
+  file.close()
+
+if args["--verbose"]:
+  echo $inputData
