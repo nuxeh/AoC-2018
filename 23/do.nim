52f1fabbf9dd3cf22f89b3a6e91bbbf56c8a452d
Edward Cragg <drq.11235@gmail.com>
2018-12-23 12:04:02 +0000

    Parse input
    pos=<0,0,0>, r=4 -> (coords: (x: 0, y: 0, z: 0), radius: 4)
    pos=<1,0,0>, r=1 -> (coords: (x: 1, y: 0, z: 0), radius: 1)
    pos=<4,0,0>, r=3 -> (coords: (x: 4, y: 0, z: 0), radius: 3)
    pos=<0,2,0>, r=1 -> (coords: (x: 0, y: 2, z: 0), radius: 1)
    pos=<0,5,0>, r=3 -> (coords: (x: 0, y: 5, z: 0), radius: 3)
    pos=<0,0,3>, r=1 -> (coords: (x: 0, y: 0, z: 3), radius: 1)
    pos=<1,1,1>, r=1 -> (coords: (x: 1, y: 1, z: 1), radius: 1)
    pos=<1,1,2>, r=1 -> (coords: (x: 1, y: 1, z: 2), radius: 1)
    pos=<1,3,1>, r=1 -> (coords: (x: 1, y: 3, z: 1), radius: 1)

---
+++ 23/do.nim
@@ -0,0 +1,69 @@
+let doc = """
+Advent of code 2018, day 23: Experimental Emergency Teleportation
+
+Usage:
+  do [options] [<input>]
+
+Options:
+  -h --help       Show this help message.
+  -v --verbose    Show extra information.
+  -t --test       Use test points
+  --part2         Process for part 2
+"""
+import re
+import docopt
+import terminal
+import streams
+import strutils
+import sequtils
+import algorithm
+import tables
+import sets
+import lists
+import typetraits
+import strformat
+
+var
+  filename = ""
+
+let args = docopt(doc, version = "0.1.0")
+if args["--test"]:
+  filename = "test.txt"
+else:
+  filename = "input.txt"
+
+if args["<input>"]:
+  filename = $args["<input>"]
+
+type
+  Xyz = object
+    x, y, z: int
+
+  Nanobot = object
+    coords: Xyz
+    radius: int
+
+var
+  file = newFileStream(filename, fmRead)
+  line = ""
+  inputData = newSeq[NanoBot]()
+
+if not isNil(file):
+  while file.readLine(line):
+    var matches: array[4, string]
+    if match(line, re"^pos=<(\d+),(\d+),(\d+)>, r=(\d+)$", matches, 0):
+      var
+        e: Nanobot
+        i = matches.map(parseInt)
+      e.coords.x = i[0]
+      e.coords.y = i[1]
+      e.coords.z = i[2]
+      e.radius = i[3]
+      inputData.add(e)
+      if args["--verbose"]:
+        echo line & " -> " & $e
+  file.close()
+
+if args["--verbose"]:
+  for e in inputData:
+    echo $e
